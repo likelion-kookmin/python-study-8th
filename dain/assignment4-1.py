29b40d5f365139464208a440b49672f47cffb5fa
dain Hong <ribbon0522@naver.com>
2020-05-05 18:29:30 +0900

    (홍다인)4주차 과제

---
+++ dain/assignment4-1.py
@@ -0,0 +1,21 @@
+class calculator:
+    def sum(self, num1, num2):
+        return num1 + num2
+    def sub(self, num1, num2):
+        return num1 - num2
+    def mul(self, num1, num2):
+        return num1*num2
+    def div(self, num1, num2):
+        return num1/num2
+    def quo(self, num1, num2):
+        return num1//num2
+    def remain(self, num1, num2):
+        return num1%num2
+
+cal=calculator()
+print(cal.sum(1,2))
+print(cal.sub(5,3))
+print(cal.mul(4,3))
+print(cal.div(100,10))
+print(cal.quo(10,2))
+print(cal.remain(10,3))
