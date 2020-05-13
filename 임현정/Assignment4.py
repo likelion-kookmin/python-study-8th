2f6308f5010b3c72c2cdf15e1d98f7354fa029fa
unknown <ijungkong@naver.com>
2020-05-13 15:56:48 +0900

    (임현정) 4주차 과제 완료

---
+++ 임현정/Assignment4.py
@@ -0,0 +1,44 @@
+class calculator:
+    def sum(self,num1,num2):
+        self.num1 = num1
+        self.num2 = num2
+        print(num1+num2)
+
+    def sub(self,num1,num2):
+        self.num1 = num1
+        self.num2 = num2
+        print(num1-num2)
+
+    def mul(self,num1,num2):
+        self.num1 = num1
+        self.num2 = num2
+        print(num1*num2)
+
+    def div(self,num1,num2):
+        self.num1 = num1
+        self.num2 = num2
+        print(num1/num2)
+
+cal = calculator()
+cal.sum(1,2)
+cal.sub(5,3)
+cal.mul(4,3)
+cal.div(100,10)
+
+print('---------can_div_zero---------')
+
+class can_div_zero(calculator):
+    def div(self,num1,num2):
+        self.num1 = num1
+        self.num2 = num2
+        if num2 == 0:
+            print('0으로는 나눌 수 없습니다.')
+        else:
+            print(num1/num2)
+
+can_div = can_div_zero()
+can_div.sum(1,2)
+can_div.sub(5,3)
+can_div.mul(4,3)
+can_div.div(100,10)
+can_div.div(10,0)
