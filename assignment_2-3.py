4f5edb9627b8180a3238f0bb301a216cfad1c724
hyeonss33 <hyeonsoo3413@gmail.com>
2020-05-16 21:02:01 +0900

    4주차 과제(이현수)

---
+++ assignment_2-3.py
@@ -0,0 +1,13 @@
+N = int(input("N :"))
+for i in range(N):
+     for j in range(N-1-i):
+         print(" ", end='')
+     for k in range(2*i+1):
+         print("*", end='' )
+     print()
+for i in range(N-1 , 0,-1):
+     for j in range(1,N+1-i):
+         print(" ", end='')
+     for k in range(2*i-1):
+         print("*", end='' )
+     print()
---
+++ sooassignment_1.py
@@ -0,0 +1,2 @@
+introduce=input("안녕하세요. 이현수입니다")
+print(introduce)
